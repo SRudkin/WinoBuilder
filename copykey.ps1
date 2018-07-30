b428a2e2e91761b58eede28f4ee51b11e2b5f8aa
SRudkin <41696952+SRudkin@users.noreply.github.com>
2018-07-30 16:14:29 -0700

    Add files via upload

---
+++ copykey.ps1
@@ -0,0 +1,5 @@
+
+#Save Recovery Key to desktop
+(Get-BitLockerVolume -MountPoint C).KeyProtector > $env:UserProfile\Desktop\BitLocker_Recovery_Key.txt
+
+rm -Force C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\recoverykey.bat
