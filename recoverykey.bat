f9b1d10ecb0ed6ce89368f03334cac782df80739
SRudkin <41696952+SRudkin@users.noreply.github.com>
2018-07-31 11:33:35 -0700

    Delete recoverykey.bat

--- recoverykey.bat
+++
@@ -1,8 +0,0 @@
-echo off
-title Firefox windows configuration tool
-
-echo Running powershell script with administrative rights
-
-PowerShell -NoProfile -NoExit -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File " "D:\setup\copykey.ps1" " ' -Verb RunAs} " 
-
-Echo Done! 
