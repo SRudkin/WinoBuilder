e8129121178c9fbd5667a0afd89662aaa82ee936
SRudkin <41696952+SRudkin@users.noreply.github.com>
2018-07-31 09:24:20 -0700

    Update firefox configuration.bat

--- firefox configuration.bat
+++ firefox configuration.bat
@@ -3,6 +3,6 @@ title Firefox windows configuration tool
 
 echo Running powershell script with administrative rights
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File " "D:\setup\test.ps1" " ' -Verb RunAs} " 
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File " "D:\setup\WinoBuilder.ps1" " ' -Verb RunAs} " 
 
 Echo Done! 
